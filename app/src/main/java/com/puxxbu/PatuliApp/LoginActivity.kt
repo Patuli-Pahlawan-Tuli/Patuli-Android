3d4f6c8f784e3ee6fdd643c05f9eddd7a5ddf499
puxxbu <pusswarog@gmail.com>
2023-05-17 21:17:07 +0700

    check fullscreen layout

--- app/src/main/java/com/puxxbu/PatuliApp/LoginActivity.kt
+++ app/src/main/java/com/puxxbu/PatuliApp/LoginActivity.kt
@@ -1,11 +1,25 @@
 package com.puxxbu.PatuliApp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        //fullscreen
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView) ?: return
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
+
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_register)
     }
 }
